bdf5f0234ab789fa343f17d807c3c64e7c458124
sajanasali123 <134501444+sajanasali123@users.noreply.github.com>
2023-06-01 23:34:10 +0530

    commited personal website
    my personal website is commited

---
+++ main.js
@@ -0,0 +1,31 @@
+let menuIcon=document.querySelector("#menu");
+let navbar=document.querySelector(".navbar");
+
+menuIcon.onclick=()=> {
+
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle('active');
+}
+
+
+
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
+window.onscroll= () => {
+    sections.forEach(sec=>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop -100;
+        let height =sec.offsetHeight;
+        let id=sec.getAttribute(id);
+
+        if(top >=offset && top <offset + height ){
+
+
+            navlinks.forEach(links =>
+                {
+                    links.classList.remove('active');
+                    document.querySelector('header nav a[href*='+   + ']').classList.add('active');
+                })
+        }
+    })
+}
